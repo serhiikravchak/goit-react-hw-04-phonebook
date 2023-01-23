341c9aaf6c29f1436056e984891c159ff9f53046
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2023-01-23 20:58:03 +0100

    updated

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -18,13 +18,13 @@ export const Contacts = ({ contactList, onDelete }) => {
     );
 }
 
-// Contacts.propTypes = {
-//   contactList: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//     })
-//   ).isRequired,
-//   onDelete: PropTypes.func.isRequired,
-// };
+Contacts.propTypes = {
+  contactList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
